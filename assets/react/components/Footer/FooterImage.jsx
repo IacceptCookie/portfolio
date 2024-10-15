903dd32ca5ba2859fcb21e3f82a8c9d0eb27bdc4
Raphaël DURAND <durandraphael.courriel@gmail.com>
2024-10-15 09:58:26 +0200

    feat: add responsive styling footer & header

--- assets/react/components/Footer/FooterImage.jsx
+++ assets/react/components/Footer/FooterImage.jsx
@@ -3,7 +3,7 @@ import {Link} from "wouter";
 
 function FooterImage({ src, alt }) {
     return (
-        <section className="footer__section">
+        <section className="footer__section image-section">
             <Link to={'/'}>
                 <img src={src} alt={alt} className="footer__image"/>
             </Link>
