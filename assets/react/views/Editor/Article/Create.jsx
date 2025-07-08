d392030969eac731b477b846d6787196d79e0496
Raphaël DURAND <durandraphael.courriel@gmail.com>
2025-07-08 20:25:29 +0200

    temporary

--- assets/react/views/Editor/Article/Create.jsx
+++ assets/react/views/Editor/Article/Create.jsx
@@ -1,11 +1,70 @@
 import React from "react";
 import "./Article.css";
 import EditorHeader from "../../../components/Editor/EditorHeader";
+import SearchBar from "../../../components/Search/SearchBar";
 
 function Create() {
+    const [searchText, updateSearchText] = React.useState("");
+
     return (
         <>
             <EditorHeader warning={true} />
+            // TODO: Déplacer les menus dans un composant (pour mutualiser avec la page d'édition
+            <form className="article-creator">
+                <div className="article-creator-menu">
+                    <div className="article-creator-menu-main">
+                        <input type="text" name="title" placeholder="Title" className="article-creator-input-title" required />
+                        <textarea name="description" placeholder="Description" className="article-creator-input-description" required />
+                        <select className="article-creator-input-categories" required>
+                            <option selected value="default">Choisir des catégories</option>
+                            <option value="value">Une catégorie</option>
+                        </select>
+                    </div>
+                    <div className="article-creator-menu-options">
+                        <div className="article-creator-menu-options-wrapper">
+                            <label>
+                                <input type="checkbox" name="privacy" value="checked" />
+                                Publique
+                            </label>
+                            <button className="article-creator-menu-preview-button">Voir l'aperçu</button>
+                        </div>
+                        <div className="article-creator-menu-tags-selector">
+                            <div className="article-creator-menu-tags-search-wrapper">
+                                <SearchBar
+                                    inputName="searchTags"
+                                    updateTextState={updateSearchText}
+                                    value={searchText}
+                                    placeholder="Chercher des tags"
+                                />
+                                <p className="article-creator-menu-tags-counter">
+                                    5 tags
+                                </p>
+                            </div>
+                            // TODO: Tag checkboxes
+                        </div>
+                        <div className="article-creator-menu-options-wrapper">
+                            <input type="file" name="thumbnails" />
+                            <img src="" alt="thumbnail preview" />
+                        </div>
+                    </div>
+                </div>
+                <div className="element-creator">
+                    <div className="element-creator-menu">
+                        // TODO: Menu de création d'élément
+                    </div>
+                    <div className="element-list">
+                        // TODO: Exemple de menu d'un élément
+                    </div>
+                </div>
+                <div className="article-creator-buttons">
+                    <button className="article-save-button">
+                        Enregistrer
+                    </button>
+                    <button className="article-delete-button">
+                        Supprimer
+                    </button>
+                </div>
+            </form>
         </>
     );
 }
--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -14,14 +14,34 @@ final class EmailController extends AbstractController
     #[Route('/email/contact', name: 'app_contact_email', methods: ['POST'])]
     public function contact(Request $request, MailerInterface $mailer): Response
     {
-        // TODO: Validate data before sending the email
         $data = json_decode($request->getContent(), true);
+
+        $requiredFields = ['firstname', 'lastname', 'email', 'subject-selector'];
+
+        foreach ($requiredFields as $field) {
+            if (empty($data[$field])) {
+                return $this->json([
+                    'error' => "field '$field' required."
+                ], Response::HTTP_BAD_REQUEST);
+            }
+        }
+
+        $firstname = strip_tags(trim($data['firstname']));
+        $lastname = strip_tags(trim($data['lastname']));
+        $email = filter_var(trim($data['email']), FILTER_SANITIZE_EMAIL);
+        $subjectSelector = strip_tags(trim($data['subject-selector']));
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return $this->json([
+                'error' => 'invalid email address'
+            ], Response::HTTP_BAD_REQUEST);
+        }
 
         // TODO: Set up a template for the Contact form email (and another one for the 2FA code)
         $email = (new Email())
             ->from('contact@raphael-durand.fr')
             ->to('durandraphael.courriel@gmail.com')
-            ->subject('Contact: '.$data['subject'].'')
+            ->subject('C: '.$data['subject-selector'].' '.$firstname.' '.$lastname)
             ->text('Votre code de vérification est : . (Ce code est valable 5 minutes)');
 
         $mailer->send($email);
